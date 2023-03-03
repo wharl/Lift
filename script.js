da6ffdaf7edebd6b385e0b5721863a2f93d26d87
vikasosmium <vikasosmium@gmail.com>
2023-03-03 21:05:34 +0530

    initial

---
+++ script.js
@@ -0,0 +1,94 @@
+'use strict'
+const floor = document.getElementById("floor")
+const lift = document.getElementById("lift")
+const start = document.getElementById("start")
+const box = document.getElementById("box")
+
+box.innerHTML = floorCreation(floor.value);
+
+// start.addEventListener("click", function () {
+//     box.innerHTML = floorCreation(floor.value);
+// });
+
+let allFloor = ``
+
+function floorCreation(floor) {
+    let allFloor = ``
+    while (floor) {
+        floor = floor - 1;
+
+        if (floor == 0) {
+            allFloor += `<div class="square"></div>`
+        }
+
+        allFloor += `<div id="num">
+        <p>Floor ${floor + 1}</p>
+        <button id="up"> Up</button><br>
+        <button id="down"> Down</button>
+        <hr>
+        </div>`;
+
+    }
+    return allFloor
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let allFloor = ``
+
+// function floorCreation(floor) {
+//     let allFloor = ``
+//     while (floor) {
+//         floor = floor - 1;
+
+//         if (floor == 0) {
+//             allFloor += `<div class="square"></div>`
+//         }
+
+//         allFloor += `<div id="num">
+//         <p>Floor ${floor + 1}</p>
+//         <button id="up"> Up</button><br>
+//         <button id="down"> Down</button>
+//         <hr>
+//         </div>`;
+
+//     }
+//     return allFloor
+// }
